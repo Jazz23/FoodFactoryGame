43a16185cb7afe199e7af2ef93354a6008994806
Jazz23 <13726053+Jazz23@users.noreply.github.com>
2026-09-12 10:29:34 -0700

    new player

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,18 +4,22 @@ using FishNet.Object;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody2D), typeof(CapsuleCollider2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(CapsuleCollider2D), typeof(SpriteRenderer))]
 public class Movement : NetworkBehaviour
 {
     [SerializeField, Min(0f)] private float moveSpeed = 4f;
+    [SerializeField, Min(0f)] private float walkFramesPerSecond = 10f;
+    [SerializeField] private Sprite[] walkSprites = null!;
 
     private InputAction _move;
     private Rigidbody2D _body;
+    private SpriteRenderer _spriteRenderer;
     private bool _isTransitioning;
 
     private void Awake()
     {
         _body = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         enabled = false;
     }
 
@@ -95,10 +99,36 @@ public class Movement : NetworkBehaviour
         }
 
         _body.MovePosition(_body.position + movement * (moveSpeed * Time.fixedDeltaTime));
+        UpdateWalkingSprite(movement);
     }
 
     public void SetTransitioning(bool value)
     {
         _isTransitioning = value;
+    }
+
+    private void UpdateWalkingSprite(Vector2 movement)
+    {
+        if (movement == Vector2.zero)
+        {
+            return;
+        }
+
+        var direction = Mathf.RoundToInt(
+            Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg / 45f);
+        var directionStart = direction switch
+        {
+            0 => 0,
+            -1 => 8,
+            -2 or 6 => 16,
+            -3 or 5 => 24,
+            4 or -4 => 32,
+            1 => 40,
+            2 => 48,
+            3 => 56,
+            _ => 0,
+        };
+        var frame = Mathf.FloorToInt(Time.time * walkFramesPerSecond) % 8;
+        _spriteRenderer.sprite = walkSprites[directionStart + frame];
     }
 }
